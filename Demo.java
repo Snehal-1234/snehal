3dd1f174f100b4f3850b706ebff39bb7201c07f3
Snehal-1234 <158546582+Snehal-1234@users.noreply.github.com>
2024-10-11 10:48:53 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,7 @@
+class Demo{
+public static void main(String args[])
+{
+  System.out.println("this java program is generated on github platfrom");
+  System.out.println("this java program executed using google app engine"); 
+}
+}
